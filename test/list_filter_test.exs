ce3d1f3d4e428e5b0680199e410b05cbc4f00793
Eduardo Piorini <eduardopiorini@hotmail.com>
2021-03-11 06:16:10 -0300

    test defined, but still not implemented

--- test/list_filter_test.exs
+++ test/list_filter_test.exs
@@ -1,6 +1,15 @@
 defmodule ListFilterTest do
   use ExUnit.Case
 
-  describe "Function must return a odd number given a list with type String on each index" do
+  describe "call/1" do
+    test "Function must return a odd number given a list with type String on each index" do
+      list = ["1", "3", "6", "43", "banana", "6", "abc"]
+
+      response = ListFilter.call(list)
+
+      expected_response = 3
+
+      assert response == expected_response
+    end
   end
 end
