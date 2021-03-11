ea76ced950b7d569f6b0bd8584e4d5b4455bde20
Eduardo Piorini <eduardopiorini@hotmail.com>
2021-03-11 06:13:23 -0300

    Module defined -> not tested yet

--- lib/list_filter.ex
+++ lib/list_filter.ex
@@ -1,4 +1,21 @@
 defmodule ListFilter do
-  def call() do
+  def call(list) do
+    list
+    |> Enum.map(fn content -> list_filter(content) end)
+    |> Enum.count(fn number -> rem(number, 2) == 1 end)
   end
+
+  defp list_filter(string) do
+    Integer.parse(string)
+    |> handle_filter()
+  end
+
+  defp handle_filter({number, _rest}), do: number
+  defp handle_filter(:error), do: 0
 end
+
+# recebe a lista
+# percorre
+# converte o indice -> sucesso? retorna numero : error
+# sucesso -> verifica se é impar
+# conta os impares
--- test/list_filter_test.exs
+++ test/list_filter_test.exs
@@ -1,8 +1,6 @@
 defmodule ListFilterTest do
   use ExUnit.Case
-  doctest ListFilter
 
-  test "greets the world" do
-    assert ListFilter.hello() == :world
+  describe "Function must return a odd number given a list with type String on each index" do
   end
 end
